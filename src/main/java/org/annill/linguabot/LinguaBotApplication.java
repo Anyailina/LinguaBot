0c139342e8e0df1801c905332358cfb2331c51c5
Ann <annill13@mail.ru>
2024-09-30 20:04:57 +0300

    initial commit

---
+++ src/main/java/org/annill/linguabot/LinguaBotApplication.java
@@ -0,0 +1,11 @@
+package org.annill.linguabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LinguaBotApplication.class, args);
+    }
+}
---
+++ src/test/java/org/annill/linguabot/LinguaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.annill.linguabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
