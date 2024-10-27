e1c71787f342323238cae940b79427146a133a62
Ann <annill13@mail.ru>
2024-10-28 01:12:51 +0300

    make refactoring

--- src/main/java/org/annill/linguabot/fabricOfAction/impl/ActionHandler.java
+++ src/main/java/org/annill/linguabot/fabricOfAction/impl/ActionHandler.java
@@ -3,20 +3,7 @@ package org.annill.linguabot.fabricOfAction.impl;
 import org.annill.linguabot.enums.ActionEnum;
 import org.annill.linguabot.states.impl.IAdd;
 
-public abstract class ActionHandler {
-    // Поле, если нужно
-    protected ActionEnum actionType;
-
-    // Конструктор
-    public ActionHandler(ActionEnum actionType) {
-        this.actionType = actionType;
-    }
-
-    // Абстрактный метод, который должен быть реализован в подклассах
-    public abstract String process(String text, long chatId, IAdd iAdd);
-
-    // Метод для получения типа действия
-    public ActionEnum getType() {
-        return actionType;
-    }
+public interface ActionHandler {
+    ActionEnum getType();
+    String process(String text, long chatId, IAdd iAdd);
 }
